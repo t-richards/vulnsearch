f9f8e79c14a457303a6ff43f6369b0297cfca414
Tom Richards <tom@tomrichards.net>
2017-02-14 18:16:08 -0500

    Fetch query string from hash

--- src/controllers/home_controller.cr
+++ src/controllers/home_controller.cr
@@ -4,12 +4,12 @@ require "kemal-session"
 module Vulnsearch
   class HomeController
     get "/" do |env|
-      query = env.params.query["q"]
+      query = env.params.query.fetch("q", "")
       render_default "home/index"
     end
 
     get "/search" do |env|
-      query = env.params.query["q"]
+      query = env.params.query.fetch("q", "")
       like_query = "%#{query}%"
       cves = Cve.from_rs(VULNDB.query("SELECT * FROM cves WHERE id LIKE ? OR summary LIKE ? ORDER BY published DESC", like_query, like_query))
 
