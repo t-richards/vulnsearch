afd2ec07638e063d8127b91d768c26120b0243d3
Tom Richards <tom@tomrichards.net>
2019-08-12 21:08:30 -0400

    Simplify test setup

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,7 +1,7 @@
 require "spec"
 
 # Use in-memory database for tests
-ENV["DATABASE_URI"] ||= "sqlite3:./db/vulnsearch_test.sqlite3"
+ENV["DATABASE_URI"] ||= "sqlite3::memory:"
 
 # Require app bits before running tests
 require "../src/app"
@@ -14,12 +14,7 @@ def database_cleaner
 end
 
 # Migrates the test database
-def migrate_test_db
-  migrator = Migrate::Migrator.new(db, logger)
-  migrator.to_latest
-end
-
-migrate_test_db
+Migrate::Migrator.new(db, logger).to_latest
 
 # Clean database before specs
 Spec.before_each do
