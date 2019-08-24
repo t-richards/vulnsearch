2d2d9b184ef292b5c147159afc5c63c13dda903f
Tom Richards <tom@tomrichards.net>
2019-08-24 19:10:22 -0400

    Lint

--- src/routes/product/search_endpoint.cr
+++ src/routes/product/search_endpoint.cr
@@ -1,23 +1,23 @@
 struct Routes::Product::SearchEndpoint
-    include Onyx::HTTP::Endpoint
+  include Onyx::HTTP::Endpoint
 
-    params do
-      form require: true do
-        type vendor : String
-        type product : String
-        type version : String
-      end
-    end
-
-    def call
-      product_id = ::Product.find_by_parts(
-        vendor: params.form.vendor.strip,
-        name: params.form.product.strip,
-        version: params.form.version.strip
-      ).id
-
-      redirect("/product/#{product_id}")
+  params do
+    form require: true do
+      type vendor : String
+      type product : String
+      type version : String
     end
   end
 
-  Onyx::HTTP.post "/search", Routes::Product::SearchEndpoint
+  def call
+    product_id = ::Product.find_by_parts(
+      vendor: params.form.vendor.strip,
+      name: params.form.product.strip,
+      version: params.form.version.strip
+    ).id
+
+    redirect("/product/#{product_id}")
+  end
+end
+
+Onyx::HTTP.post "/search", Routes::Product::SearchEndpoint
