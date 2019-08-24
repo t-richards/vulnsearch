2c99e72b7f17656207b6e48a58db825966e5d34f
Tom Richards <tom@tomrichards.net>
2019-08-24 16:20:48 -0400

    Genericize

--- public/src/application.ts
+++ public/src/application.ts
@@ -1,42 +1,63 @@
-async function handleVendorInput(evt: Event): Promise<void> {
-    if (evt.target === null) {
-        return;
-    }
+(function() {
+    type TypeaheadData = Promise<Array<string>>;
 
-    const target = evt.target as HTMLInputElement;
-    const body = {
-        vendor: target.value
-    };
-
-    const response = await fetch('/vendor', {
-        method: 'POST',
-        body: JSON.stringify(body),
-        headers: {
-            'Content-Type': 'application/json'
+    function wireTypeahead(
+        inputId: string,
+        listId: string,
+        dataCallback: (value: string) => TypeaheadData
+    ) {
+        const input = document.getElementById(inputId);
+        if (input !== null) {
+            const handler = createInputHandler(listId, dataCallback);
+            input.addEventListener("input", handler);
         }
-    });
-    const jsonResponse = await response.json();
-    const data = jsonResponse.vendors as Array<string>;
-
-    const newList = document.createElement('datalist');
-    newList.id = 'vendor-list';
-
-    data.forEach((vendor) => {
-        let opt = document.createElement('option');
-        opt.value = vendor;
-        opt.innerHTML = vendor;
-        newList.appendChild(opt);
-    });
-
-    const originalList = document.getElementById('vendor-list');
-    if (originalList === null || originalList.parentNode === null) {
-        console.error("Failed to replace original list.");
-        return;
     }
-    originalList.parentNode.replaceChild(newList, originalList);
-};
 
-const vendorInput = document.getElementById('vendor');
-if (vendorInput !== null) {
-    vendorInput.addEventListener('input', handleVendorInput);
-}
+    function createInputHandler(
+        listId: string,
+        dataCallback: (value: string) => TypeaheadData
+    ) {
+        return async function(evt: Event): Promise<void> {
+            if (evt.target === null) {
+                return;
+            }
+            const target = evt.target as HTMLInputElement;
+            const data = await dataCallback(target.value);
+            const newList = document.createElement("datalist");
+            newList.id = listId;
+
+            data.forEach(item => {
+                let opt = document.createElement("option");
+                opt.value = item;
+                opt.innerHTML = item;
+                newList.appendChild(opt);
+            });
+
+            const originalList = document.getElementById(listId);
+            if (originalList === null || originalList.parentNode === null) {
+                console.error("Failed to replace original list.");
+                return;
+            }
+            originalList.parentNode.replaceChild(newList, originalList);
+        };
+    }
+
+    async function vendorCallback(value: string): TypeaheadData {
+        const body = {
+            vendor: value
+        };
+
+        const response = await fetch("/vendor", {
+            method: "POST",
+            body: JSON.stringify(body),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+        const jsonResponse = await response.json();
+        const data = jsonResponse.vendors as Array<string>;
+        return data;
+    }
+
+    wireTypeahead("vendor", "vendor-list", vendorCallback);
+})();
