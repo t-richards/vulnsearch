94c4c1e41213d40e3d7ef5cfe32b9880845f880c
Tom Richards <tom@tomrichards.net>
2019-08-25 12:13:13 -0400

    Reorder columns

--- db/migrations/20190812205546_add_products.sql
+++ db/migrations/20190812205546_add_products.sql
@@ -1,13 +1,13 @@
 -- +migrate Up
 CREATE TABLE IF NOT EXISTS products (
   id      INTEGER PRIMARY KEY,
-  name    VARCHAR(255) NOT NULL,
   vendor  VARCHAR(255) NOT NULL,
+  name    VARCHAR(255) NOT NULL,
   version VARCHAR(255) NOT NULL
 );
 
 CREATE UNIQUE INDEX IF NOT EXISTS products_unique
-  ON products (name, vendor, version);
+  ON products (vendor, name, version);
 
 -- +migrate Down
 DROP TABLE IF EXISTS products;
