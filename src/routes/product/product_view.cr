2fe2026af92427d98b693e582480234a038add14
Tom Richards <tom@tomrichards.net>
2019-09-11 21:05:29 -0400

    Move script and template bits

--- src/routes/product/product_view.cr
+++ src/routes/product/product_view.cr
@@ -2,6 +2,7 @@ struct Routes::Product::ProductView
   include Onyx::HTTP::View
 
   def initialize(@product : ::Product, @cves : Array(Cve))
+    @full_name = "#{@product.vendor} #{@product.name} #{@product.version}"
   end
 
   html_template("product.html.ecr")
--- src/routes/routes.cr
+++ src/routes/routes.cr
@@ -1,19 +1,6 @@
 require "onyx/http"
 
-def render_with_layout(io, &block)
-  ECR.embed("#{__DIR__}/../layouts/application.html.ecr", io)
-end
-
-module Onyx::HTTP::View
-  macro html_template(template)
-    define_type_renderer(render_to_text_html, "text/html", {"text/html"}) do
-      render_with_layout(io) do
-        ECR.embed("#{__DIR__}/#{{{template}}}", io)
-        nil
-      end
-    end
-  end
-end
+require "./template"
 
 require "./home/*"
 require "./product/*"
