e9c724b024ae0ace90a335bca6d7df24c52baeb7
Tom Richards <tom@tomrichards.net>
2020-01-17 00:00:46 -0500

    Remove vendored bootstrap

--- src/routes/template.cr
+++ src/routes/template.cr
@@ -1,3 +1,5 @@
+require "sass"
+
 def render_with_layout(io, &block)
   ECR.embed("#{__DIR__}/../layouts/application.html.ecr", io)
 end
@@ -11,4 +13,20 @@ module Onyx::HTTP::View
       end
     end
   end
+end
+
+def compile_scss(path)
+  css = Sass.compile_file(
+    path,
+    include_path: "node_modules/bootstrap/scss",
+    output_style: Sass::OutputStyle::EXPANDED,
+    precision: 8,
+    output_path: "/dist",
+    source_map_file: "/dist/application.css.map"
+  )
+  base = File.basename(path, ".scss")
+  out_path = File.join("public/dist", base) + ".css"
+  File.write(out_path, css)
+
+  css
 end
